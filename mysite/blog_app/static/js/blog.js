ad22453d851431db200e49381317a2f362f1a96f
ancilnf <ancil.newton@outlook.com>
2022-01-01 21:43:41 +0530

    Blog Project

--- mysite/blog_app/static/js/blog.js
+++ mysite/blog_app/static/js/blog.js
@@ -55,4 +55,49 @@ window.addEventListener('load',function(){
         }
             localStorage.setItem('btn_checked', el.target.checked);
     }); 
+
+// WEATHER API FETCH
+
+    const location = document.getElementById("locationData");
+    location.addEventListener('submit', (event) =>{
+        event.preventDefault();
+
+        const city= location.elements['city'].value
+        console.log(city)
+    
+        weatherBalloon(city)
+})
 });
+
+function weatherBalloon(cityID) {
+    var key = '60d7a2f1a6ebc07803526f75beb458ac';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityID}&units=metric&APPID=${key}`)
+    .then(res=> res.json())
+    .then(function (data){
+        console.log(data)
+        const weatherDescription = data.weather[0].description;
+        document.getElementById('description').innerHTML = weatherDescription;
+
+        const temp = (data.main.temp)
+        const temp_max = (data.main.temp_max)
+        const temp_min = (data.main.temp_min)
+
+        document.getElementById('temp').innerHTML =`Temp: ${Math.trunc(temp)}${`&deg;`}C`;
+        document.getElementById('temp-max').innerHTML = `Max Temp: ${Math.trunc(temp_max)}${`&deg;`}C`;
+        document.getElementById('temp-min').innerHTML = `Min Temp: ${Math.trunc(temp_min)}${`&deg;`}C`;
+	    document.getElementById('location').innerHTML = `${(data.name)} ${(data.sys.country)}`;
+        document.getElementById('openweathercredits').innerHTML = 'Data provided by <span> OpenWeather </span>';
+
+    })
+      .catch((error) => {
+          console.error("GET ERROR", error);
+          document.getElementById('description').innerHTML = "Incorrect City name. Please try again.";
+          document.getElementById('temp').innerHTML ='';
+          document.getElementById('temp-max').innerHTML = '';
+          document.getElementById('temp-min').innerHTML = '';
+	      document.getElementById('location').innerHTML = '';
+          document.getElementById('openweathercredits').innerHTML = '';
+      });
+      
+          //catch errors
+    }
