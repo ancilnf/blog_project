55717cbe3aab6978cba3e1231870e7649e374e12
ancilnf <ancil.newton@outlook.com>
2022-03-12 22:11:47 +0530

    Blog Project

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'blog_app.apps.BlogAppConfig'
+    'blog_app.apps.BlogAppConfig',
     'rest_framework'
 ]
 
